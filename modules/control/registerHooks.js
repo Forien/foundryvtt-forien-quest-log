8a832baffd9062b3d35ad722a189b197114a4628
typhonrt <support@typhonjs.io>
2021-07-03 04:05:33 -0700

    Pull macro image from new `giverData` field instead of enriching.

--- modules/control/registerHooks.js
+++ modules/control/registerHooks.js
@@ -60,8 +60,7 @@ export default function registerHooks()
             command
          };
 
-         macroData.img = quest.splashAsIcon && quest.splash.length ? quest.splash :
-          (await Enrich.giverFromQuest(quest))?.img;
+         macroData.img = quest.splashAsIcon && quest.splash.length ? quest.splash : quest.giverData.img;
 
          let macro = game.macros.contents.find((m) => (m.data.command === command));
          if (!macro)
