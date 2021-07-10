b997d801fbadb8b13b6bb9072d17005598a37735
typhonrt <support@typhonjs.io>
2021-07-10 09:14:21 -0700

    fixed external tags

--- src/typedefs.js
+++ src/typedefs.js
@@ -1,5 +1,11 @@
+/* eslint-disable jsdoc/valid-types */  // ESDoc uses a non-conformant external tag.
+
 /**
  * @external {jQuery} https://api.jquery.com/
- *
+ */
+
+/**
  * @external {sessionStorage} https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
  */
+
+/* eslint-enable jsdoc/valid-types */
