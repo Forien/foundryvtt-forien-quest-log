71035f88a4fc95a84b8602cb431611d43e2f4835
typhonrt <support@typhonjs.io>
2022-08-24 02:50:26 -0700

    load TinyMCE v5 or v6 plugin.

--- src/init.js
+++ src/init.js
@@ -1,5 +1,33 @@
 import FQLHooks   from './control/FQLHooks.js';
-import '../external/typhonjs-oembed.js';        // Loads the TyphonJS oEmbed TinyMCE plugin
 
 // Initialize all hooks
 FQLHooks.init();
+
+/**
+ * Handle loading the TyphonJS oEmbed TinyMCE plugin based on Foundry version.
+ *
+ * v9 of Foundry ships with TinyMCE v5.
+ * v10 of Foundry ships with TinyMCE v6.
+ */
+Hooks.once('init', async () =>
+{
+   const isV10 = !foundry.utils.isNewerVersion(10, game.version ?? game.data.version);
+
+   try
+   {
+      if (isV10)
+      {
+         // Load oEmbed TinyMCE v6 plugin.
+         await import('../external/typhonjs-oembed-v6.js');
+      }
+      else
+      {
+         // Load oEmbed TinyMCE v5 plugin.
+         await import('../external/typhonjs-oembed-v5.js');
+      }
+   }
+   catch (err)
+   {
+      console.warn(`ForienQuestLog warning: Failed to load TyphonJS oEmbed plugin.`);
+   }
+});
