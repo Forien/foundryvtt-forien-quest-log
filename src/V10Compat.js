5aa5a4a28a6a4cdd5126ec24064d18ad189f6c4a
typhonrt <support@typhonjs.io>
2022-08-26 10:48:44 -0700

    Added token and journal image compatibility support.

--- src/V10Compat.js
+++ src/V10Compat.js
@@ -2,7 +2,7 @@ let isV10 = false;
 
 Hooks.once('init', () =>
 {
-   isV10 = !foundry.utils.isNewerVersion(10, game.version ?? game.data.version);
+   isV10 = !foundry.utils.isNewerVersion(10, game.version ?? game?.data?.version);
 });
 
 /**
@@ -43,7 +43,6 @@ export class V10Compat
     */
    static get isV10() { return isV10; }
 
-
    /**
     * Returns the data property depending on v10.
     *
@@ -51,7 +50,7 @@ export class V10Compat
     *
     * @param {string}   property - Property field.
     *
-    * @returns {*} Data value.
+    * @returns {string} Data value.
     */
    static get(doc, property)
    {
@@ -60,13 +59,55 @@ export class V10Compat
    }
 
    /**
+    * Returns any associated journal image. For v10 journal docs this is the first page that is an image.
+    *
     * @param {foundry.abstract.Document|Document}  doc -
     *
-    * @returns {*} Foundry ownership / permission object.
+    * @returns {string} Journal image.
+    */
+   static journalImage(doc)
+   {
+      if (!doc) { return void 0; }
+
+      if (isV10)
+      {
+         // Search for the first JournalEntryPage embedded collection for an image.
+         try
+         {
+            const pages = doc.getEmbeddedCollection('JournalEntryPage');
+            for (const page of pages)
+            {
+               if (page?.type === 'image') { return page?.src; }
+            }
+         }
+         catch (err) { return void 0; }
+      }
+      else
+      {
+         return doc?.data?.img;
+      }
+   }
+
+   /**
+    * @param {foundry.abstract.Document|Document}  doc -
+    *
+    * @returns {string} Foundry ownership / permission object.
     */
    static ownership(doc)
    {
       if (!doc) { return void 0; }
       return isV10 ? doc.ownership : doc.data.permission;
+   }
+
+   /**
+    * @param {foundry.abstract.Document|Document}  doc -
+    *
+    * @returns {string} Token image path.
+    */
+   static tokenImg(doc)
+   {
+      if (!doc) { return void 0; }
+
+      return V10Compat.isV10 ? doc?.prototypeToken?.texture?.src : doc?.data?.token?.img;
    }
 }
